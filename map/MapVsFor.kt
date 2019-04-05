ca77d5d1ac3e8c195bc58810256384d29815581e
Matt Moore <mattmoore@carbonhelix.com>
2019-04-04 20:45:56 -0500

    foreach vs map

---
+++ map/MapVsFor.kt
@@ -0,0 +1,30 @@
+data class Author(val firstName: String, val lastName: String)
+
+data class Book(val title: String, val author: Author)
+
+val books = listOf(
+  Book("Microservices Architecture", Author("Chris", "Richardson")),
+  Book("Kotlin in Action", Author("Dmitry", "Jemerov"))
+)
+
+fun getAuthorsWithForLoop(books: List<Book>): List<Author> {
+  var authors = mutableListOf<Author>()
+  for (x in books) {
+    authors.add(x.author)
+  }
+  return authors
+}
+
+fun getAuthorsWithMap(books: List<Book>): List<Author> {
+  return books.map { it.author }
+}
+
+fun main() {
+  // Want to find books written by Chris Richardson.
+
+  // for loop
+  println(getAuthorsWithForLoop(books))
+
+  // map
+  println(getAuthorsWithMap(books))
+}
